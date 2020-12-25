3442169de14ab36cda28d1020dc4fba44b171ca2
Tomas Pereira de Vasconcelos <tomas@tiqets.com>
2020-12-25 06:50:12 +0000

    update push.sh

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 cd src/ && \
   mkdocs build && \
-  echo -n "tomasvasconcelos.com" > ../docs/CNAME
+  printf "tomasvasconcelos.com" > ../docs/CNAME
   git ca "mkdocs build" && \
   git push
